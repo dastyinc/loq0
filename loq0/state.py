9eeb354077008517f18a52f71c04b7f2f1fbba72
Seohyun Lee <studioseorii@gmail.com>
2023-07-11 22:34:46 +0900

    feat: check if game is able to be finished

--- loq0/state.py
+++ loq0/state.py
@@ -68,52 +68,76 @@ class State:
         return False
 
     def position(self):
-        return (self._st[0, 0], self._st[0, 1]), (self._st[1, 0], self._st[1, 1])
+        pl = self.player()
+        return (self._st[pl, 0], self._st[pl, 1]), (self._st[1 - pl, 0], self._st[1 - pl, 1])
 
-    def _move(self, pos):
+    def _move(self, pos, ppos=None, opos=(0, 0)):
         x, y = pos
+        if not ppos:
+            (px, py), (ox, oy) = self.position()[0]
+        else:
+            px, py = ppos
+            ox, oy = opos
         if x < 1 or x > BOARD_SIZE or y < 1 or y > BOARD_SIZE:
             return False
-        dis = abs(self._st[self.player(), 0] - x) + abs(self._st[self.player(), 1] - y)
+        dis = abs(px - x) + abs(py - y)
         if dis > 2 or dis == 0:
             return False
         elif dis == 1:
-            if x == self._st[self.player(), 0]:
-                if self.vertical_block(x, min(y, self._st[self.player(), 1])):
+            if x == px:
+                if self.vertical_block(x, min(y, py)):
                     return False
-            elif self.horizontal_block(min(x, self._st[self.player(), 0]), y):
+            elif self.horizontal_block(min(x, px), y):
                 return False
         else:
-            if x == self._st[self.player(), 0]:
-                yt = self._st[self.opponent(), 1]
-                if self._st[self.opponent(), 0] != x or yt * 2 != y + self._st[self.player(), 1]:
+            if x == px:
+                yt = oy
+                if ox != x or yt * 2 != y + py:
                     return False
                 if self.vertical_block(x, yt) or self.vertical_block(x, yt + 1):
                     return False
-            elif y == self._st[self.player(), 1]:
-                xt = self._st[self.opponent(), 0]
-                if self._st[self.opponent(), 1] != y or xt * 2 != x + self._st[self.player(), 0]:
+            elif y == py:
+                xt = ox
+                if oy != y or xt * 2 != x + px:
                     return False
                 if self.horizontal_block(xt, y) or self.horizontal_block(xt + 1, y):
                     return False
-            elif self._st[self.player(), 0] == self._st[self.opponent(), 0] and y == self._st[self.opponent(), 1]:
-                if abs(x - self._st[self.player(), 0]) != 1 or \
-                        self.vertical_block(self._st[self.player(), 0], min(y, self._st[self.player(), 1])) or \
-                        self.horizontal_block(min(x, self._st[self.player(), 0]), y) or \
-                        not self.vertical_block(self._st[self.player(), 0],
-                                                y + 1 if y > self._st[self.player(), 1] else y - 1):
+            elif px == ox and y == oy:
+                if abs(x - px) != 1 or \
+                        self.vertical_block(px, min(y, py)) or \
+                        self.horizontal_block(min(x, px), y) or \
+                        not self.vertical_block(px,
+                                                y + 1 if y > py else y - 1):
                     return False
-            elif self._st[self.player(), 1] == self._st[self.opponent(), 1] and x == self._st[self.opponent(), 0]:
-                if abs(y - self._st[self.player(), 1]) != 1 or \
-                        self.vertical_block(x, min(y, self._st[self.player(), 1])) or \
-                        self.horizontal_block(min(x, self._st[self.player(), 0]), y) or \
-                        not self.horizontal_block(x + 1 if x > self._st[self.player(), 0] else x - 1,
-                                                  self._st[self.player(), 1]):
+            elif py == oy and x == ox:
+                if abs(y - py) != 1 or \
+                        self.vertical_block(x, min(y, py)) or \
+                        self.horizontal_block(min(x, px), y) or \
+                        not self.horizontal_block(x + 1 if x > px else x - 1, py):
                     return False
             else:
                 return False
 
-        if self._st[self.opponent(), 0] == x and self._st[self.opponent(), 1] == y and not self.internal():
+        if ox == x and oy == y and not self.internal():
             return False
 
         return True
+
+    def _endable(self):
+        visited = np.zeros((BOARD_SIZE, BOARD_SIZE), dtype=bool)
+        stk = [self.position()[0]]
+        dx = [0, 0, 1, -1]
+        dy = [1, -1, 0, 0]
+        visited[stk[0][0], stk[0][1]] = True
+        while len(stk) > 0:
+            x, y = stk.pop()
+            for i in range(1, 5):
+                if not visited[x + dx[i] - 1, y + dy[i] - 1] and self._move((x + dx[i], y + dy[i]), self.position()[0]):
+                    if self.player() == 0 and x + dx[i] == BOARD_SIZE:
+                        return True
+                    elif self.player() == 1 and y + dy[i] == BOARD_SIZE:
+                        return True
+                    visited[x - 1, y - 1] = True
+                    stk.append((x + dx[i], y + dy[i]))
+
+        return False
